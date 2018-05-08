868ccab8a955277fa465144e73e74437cb9270f7
Luciany Laurentino <lucylaurentino@gmail.com>
2018-05-09 11:40:36 +1200

    Isues with user input

--- Program.cs
+++ Program.cs
@@ -16,33 +16,29 @@ namespace personClass
             LastName = lastname;
             YearOfBirth = yearofbirth;
         }
-
-        public string getFullName(){ 
+        public string getFullName()
+        { 
             return $"{FirstName} {LastName}";
-            }
+        }
         public int getAge()
         {
             return DateTime.Now.Year - this.YearOfBirth;
         }
-            public string PersonInfo(){
+        public string PersonInfo()
+        {
             var personInfo = "";            
             personInfo += $"First Name: {FirstName}\n";
             personInfo += $"Last Name: {LastName}\n\n";
-           return personInfo;
+            return personInfo;
         }
     }
+    
     class Program
     {
-        private static DateTime DayOfYear;
-        public static DateTime YearOfBirth;
-        private static bool answer;
-
         static void Main(string[] args)
         {
             //Star the program with Clear()
             Console.Clear();
-
-            bool y = true;
 
             Console.WriteLine("Enter name of new person bellow");
             Console.Write("First Name: Jane \n");
@@ -59,28 +55,38 @@ namespace personClass
             Console.WriteLine("Enter year of birth: 2001 \n");
             Person person2 = new Person ("John", "Smith", 2001);
             Console.WriteLine($"Person: {person2.getFullName()} is {person2.getAge()} years old.");
-            
-            Console.Write("Do you want to enter another person?<y/n> \n");
+        
+            var checkAnswer = true;
+
+            do
+            {              
+            Console.Write("Do you want to enter another person?<y/n> ");
             var answer = Console.ReadLine();
-            if(answer = y)
+            if(answer == "y")
             {
+               checkAnswer = true; 
+            }else{
+                checkAnswer = false;
+
+            } if(checkAnswer){
             Console.WriteLine("Enter name of new person bellow");
             Console.Write("First Name: ");
-            var firstname = Console.ReadLine();
+            string firstname = Console.ReadLine();
             Console.Write("Last Name: ");
-            var lastname = Console.ReadLine();
+            string lastname = Console.ReadLine();
             Console.WriteLine();
             Console.WriteLine("Greeting new person record...\n");
             Console.Write("Enter year of birth: ");
             var yearofbirth = Console.ReadLine();
-            Person person3 = new Person ("", "", 1972);
+            Person person3 = new Person ("", "", 2000);
             Console.WriteLine($"Person: {person3.getFullName()}  is {person3.getAge()} years old. \n");
-            Console.WriteLine("Press any key to exit");
-            Console.ReadKey();
-
-            } else {Console.WriteLine("Press any key to exit");}
+            Console.ReadKey();             
+            }
+            } while (checkAnswer == true);
+            
+                      
         }
-    }
+    }                   
 }
 
 
