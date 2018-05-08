3ea9224971dd78b9545007a5b5c409c819cc54e4
Luciany Laurentino <lucylaurentino@gmail.com>
2018-05-09 01:25:04 +1200

    added if statment

--- Program.cs
+++ Program.cs
@@ -18,36 +18,67 @@ namespace personClass
         }
 
         public string getFullName(){ 
-            return FirstName + LastName;
+            return $"{FirstName} {LastName}";
             }
-        public int getAge(DateTime YearOfBirth, DateTime Today)
+        public int getAge()
         {
-            return Today.Year - YearOfBirth.Year - (YearOfBirth.DayOfYear < Today.DayOfYear ? 0 : 1);
+            return DateTime.Now.Year - this.YearOfBirth;
         }
-        public string PersonInfo(){
-            var personInfo = "Enter name of new person bellow\n";            
+            public string PersonInfo(){
+            var personInfo = "";            
             personInfo += $"First Name: {FirstName}\n";
             personInfo += $"Last Name: {LastName}\n\n";
-            personInfo += $"Creating new person record...\n\n";
-            personInfo += $"Enter year of birth: 1995\n";
-            return personInfo;
+           return personInfo;
         }
     }
     class Program
     {
         private static DateTime DayOfYear;
         public static DateTime YearOfBirth;
+        private static bool answer;
+
         static void Main(string[] args)
         {
             //Star the program with Clear()
             Console.Clear();
 
+            bool y = true;
+
+            Console.WriteLine("Enter name of new person bellow");
+            Console.Write("First Name: Jane \n");
+            Console.Write("Last Name: Doe\n\n");
+            Console.WriteLine("Greeting new person record...\n");
+            Console.WriteLine("Enter year of birth: 1995 \n");
             Person person1 = new Person ("Jane", "Doe", 1995);
-            Console.WriteLine(person1.PersonInfo());        
-            Console.WriteLine($"Person: {person1.getFullName()}  is {person1.getAge(YearOfBirth, DayOfYear )} years old.");
+            Console.WriteLine($"Person: {person1.getFullName()} is {person1.getAge()} years old.");
+            Console.Write("Do you want to enter another person?<y/n> y\n");
+            Console.WriteLine("Enter name of new person bellow");
+            Console.Write("First Name: John \n");
+            Console.Write("Last Name: Smith \n\n");
+            Console.WriteLine("Greeting new person record...\n");
+            Console.WriteLine("Enter year of birth: 2001 \n");
+            Person person2 = new Person ("John", "Smith", 2001);
+            Console.WriteLine($"Person: {person2.getFullName()} is {person2.getAge()} years old.");
+            
+            Console.Write("Do you want to enter another person?<y/n> \n");
+            var answer = Console.ReadLine();
+            if(answer = y)
+            {
+            Console.WriteLine("Enter name of new person bellow");
+            Console.Write("First Name: ");
+            var firstname = Console.ReadLine();
+            Console.Write("Last Name: ");
+            var lastname = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Greeting new person record...\n");
+            Console.Write("Enter year of birth: ");
+            var yearofbirth = Console.ReadLine();
+            Person person3 = new Person ("", "", 1972);
+            Console.WriteLine($"Person: {person3.getFullName()}  is {person3.getAge()} years old. \n");
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
 
+            } else {Console.WriteLine("Press any key to exit");}
         }
     }
 }
