9f0b2d2aec1f74704e8bc0e516579cfdaf79140f
Luciany Laurentino <lucylaurentino@gmail.com>
2018-05-10 14:27:15 +1200

    code working

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace personClass
 {
@@ -27,9 +26,9 @@ namespace personClass
         }
         public string PersonInfo()
         {
-            var personInfo = "";            
+            var personInfo = "Enter name of new person bellow \n";            
             personInfo += $"First Name: {FirstName}\n";
-            personInfo += $"Last Name: {LastName}\n\n";
+            personInfo += $"Last Name: {LastName}\n";
             return personInfo;
         }
     }
@@ -41,21 +40,17 @@ namespace personClass
             //Star the program with Clear()
             Console.Clear();
 
-            Console.WriteLine("Enter name of new person bellow");
-            Console.Write("First Name: Jane \n");
-            Console.Write("Last Name: Doe\n\n");
-            Console.WriteLine("Greeting new person record...\n");
-            Console.WriteLine("Enter year of birth: 1995 \n");
-            Person person1 = new Person ("Jane", "Doe", 1995);
-            Console.WriteLine($"Person: {person1.getFullName()} is {person1.getAge()} years old.");
+            Person person = new Person ("Jane", "Doe", 1995);
+            Console.WriteLine(person.PersonInfo());
+            Console.WriteLine("Creating new person record...\n");
+            Console.WriteLine("Enter year of birth: 1995 \n");         
+            Console.WriteLine($"Person: {person.getFullName()} is {person.getAge()} years old.");
             Console.Write("Do you want to enter another person?<y/n> y\n");
-            Console.WriteLine("Enter name of new person bellow");
-            Console.Write("First Name: John \n");
-            Console.Write("Last Name: Smith \n\n");
-            Console.WriteLine("Greeting new person record...\n");
+            Person person1 = new Person ("John", "Smith", 2001);
+            Console.WriteLine(person1.PersonInfo());
+            Console.WriteLine("Creating new person record...\n");
             Console.WriteLine("Enter year of birth: 2001 \n");
-            Person person2 = new Person ("John", "Smith", 2001);
-            Console.WriteLine($"Person: {person2.getFullName()} is {person2.getAge()} years old.");
+            Console.WriteLine($"Person: {person1.getFullName()} is {person1.getAge()} years old.");
         
             var checkAnswer = true;
             
@@ -76,11 +71,11 @@ namespace personClass
             Console.Write("Last Name: ");
             var lastname = Console.ReadLine();
             Console.WriteLine();
-            Console.WriteLine("Greeting new person record...\n");
+            Console.WriteLine("Creating new person record...\n");
             Console.Write("Enter year of birth: ");
             int userInput = int.Parse(Console.ReadLine());
-            Person person3 = new Person ("", "", 2002);
-            Console.WriteLine($"Person: {person3.getFullName()}  is {person3.getAge()} years old. \n");
+            Person person2 = new Person (firstname, lastname, userInput);
+            Console.WriteLine($"Person: {person2.getFullName()}  is {person2.getAge()} years old. \n");
             Console.ReadKey();             
             }
             } while (checkAnswer == true);
